8881b231df2ab3a4f6d22519dd88b491fed83b15
tannv_57 <NVantan94@gmail.com>
2020-11-12 17:15:51 +0900

    add Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build the MERN TINDER clone guys!!!</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>I'm header</h2>
+        </div>
+    )
+}
+
+export default Header
