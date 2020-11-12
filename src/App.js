7003b7d78c1d3abee51e13a62d75ed197d83686a
tannv_57 <NVantan94@gmail.com>
2020-11-12 18:06:23 +0900

    add tinder header component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/header/Header';
 function App() {
   return (
     <div className="app">
-      <h1>Lets build the MERN TINDER clone guys!!!</h1>
       <Header />
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+
+import IconButton from "@material-ui/core/IconButton";
+
+import './Header.css';
 
 function Header() {
-    return (
-        <div className="header">
-            <h2>I'm header</h2>
-        </div>
-    )
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon 
+            fontSize="large"
+            className="header__icon" />
+      </IconButton>
+
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt=""
+        />
+
+      <IconButton>
+        <ForumIcon fontSize='large'
+          className="header__icon"
+        />
+      </IconButton>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
