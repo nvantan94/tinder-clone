93de735328e89d30656cba049cf9c6169bf7c165
tannv_57 <NVantan94@gmail.com>
2020-11-13 17:13:16 +0900

    add db configs and api endpoints

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cards from './dbCards.js'
 
 const app = express();
 const port = process.env.PORT || 8001;
@@ -14,6 +15,28 @@ mongoose.connect(connection_url, {
 
 app.get('/', (req, res) => {
   res.status(200).send('HELLO CLEVER PROGRAMMER!!!');
+});
+
+app.post('/tinder/cards', (req, res) => {
+  const dbCard = req.body;
+
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+});
+
+app.get('/tinder/cards', (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
 })
 
 app.listen(port, () => console.log(`listening on localhost: ${port}`))
