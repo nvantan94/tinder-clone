75ff05ca9e512377c5ed974d223eb6419c03d7a4
tannv_57 <NVantan94@gmail.com>
2020-11-13 16:13:56 +0900

    add SwipeButtons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/header/Header';
 import TinderCards from './components/TinderCards/TinderCards';
+import SwipeButtons from './components/SwipeButtons/SwipeButtons'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
